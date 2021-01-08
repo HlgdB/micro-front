596bd0b961c9dcac23181a11f3d32963b2b150ef
alexiusll <linkenzone@163.com>
2021-01-08 17:36:06 +0800

    更新图片处理

--- src/components/PhotoTool/index.tsx
+++ src/components/PhotoTool/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 
@@ -7,26 +8,321 @@
  * @Date: 2021-01-07 15:59:08
  */
 
-import React, { useEffect, useState } from 'react';
+import { Button, Divider } from 'antd';
+import React, { useEffect, useState, useRef } from 'react';
 import { connect, Dispatch } from 'umi';
 import styles from './style.less';
+import pic1 from '@/pages/static/pic1.png';
 
 interface PhotoToolProps {
   dispatch: Dispatch;
 }
 
+// 坐标点的数据结构
+interface Point {
+  x: number;
+  y: number;
+}
+
 const PhotoTool: React.FC<PhotoToolProps> = (props) => {
   const { dispatch } = props;
 
+  const test_p1: Point = { x: 10, y: 20 };
+
+  // 画布
+  const canvasRef: any = useRef(null);
+  const contextRef: any = useRef(null);
+  const drawingBoardRef: any = useRef(null);
+  const picRef: any = useRef(null);
+
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [canvansSize, setCanvansSize] = useState({ width: 0, height: 0 });
+
+  const [tool, setTool] = useState<number | null>(null);
+
+  const [p_list, setP_list] = useState<Point[]>([]);
+
+  const [scale, setScale] = React.useState({ x: 1, y: 1 });
+
+  // 初始化函数
+  useEffect(() => {
+    const canvas: any = canvasRef.current;
+
+    let context;
+    if (canvas) {
+      context = canvas.getContext('2d');
+
+      context.lineCap = 'round';
+      context.strokeStyle = 'black';
+      context.lineWidth = 5;
+
+      contextRef.current = context;
+    } else {
+      console.log('未找到canvasRef');
+    }
+  }, []);
+
+  // #region canvas帮助函数
+
+  /**
+   * @description: 通过坐标绘制点
+   * @Param:
+   * @param {Point} p
+   */
+  const drawPoint = (p: Point) => {
+    contextRef.current.beginPath();
+    // 圆的坐标 x , y ,圆的半径， 圆的起始角度和结束角度
+    contextRef.current.arc(p.x, p.y, 4, 0, Math.PI * 2);
+    contextRef.current.fill();
+    contextRef.current.closePath();
+  };
+
+  /**
+   * @description: 通过一系列坐标点，绘制点
+   * @Param:
+   * @param {Point} _p_list
+   */
+  const drawPointsLineByList = (_p_list: Point[]) => {
+    if (_p_list.length > 0) {
+      for (let i = 0; i < _p_list.length; ++i) {
+        contextRef.current.beginPath();
+        contextRef.current.arc(_p_list[i].x, _p_list[i].y, 4, 0, Math.PI * 2);
+        contextRef.current.fill();
+      }
+    }
+
+    contextRef.current.closePath();
+  };
+
+  /**
+   * @description: 通过一系列坐标点，绘制直线
+   * @Param:
+   * @param {Point} _p_list
+   */
+  const drawStraightLineByList = (_p_list: Point[]) => {
+    contextRef.current.beginPath();
+
+    if (_p_list.length > 0) {
+      contextRef.current.lineWidth = 0.6;
+      contextRef.current.strokeStyle = '#111';
+      contextRef.current.moveTo(_p_list[0].x, _p_list[0].y);
+      for (let i = 1; i < _p_list.length; ++i) {
+        contextRef.current.lineTo(_p_list[i].x, _p_list[i].y);
+      }
+
+      contextRef.current.stroke();
+    }
+
+    contextRef.current.closePath();
+  };
+
+  /**
+   * @description: 通过一系列坐标点，绘制平滑曲线
+   * @Param:
+   * @param {Point} _p_list
+   */
+  const drawSmoothLineByList = (_p_list: Point[]) => {
+    contextRef.current.beginPath();
+    console.log('p_list', _p_list);
+
+    if (_p_list.length > 0) {
+      contextRef.current.lineWidth = 3;
+      contextRef.current.strokeStyle = '#eee';
+      contextRef.current.moveTo(_p_list[0].x, _p_list[0].y);
+
+      let { x } = _p_list[0];
+      let { y } = _p_list[0];
+
+      for (let i = 0; i < _p_list.length; ++i) {
+        const x2 = _p_list[i].x;
+        const y2 = _p_list[i].y;
+
+        const mx = (x + x2) / 2;
+        const my = (y + y2) / 2;
+
+        // 创建二次贝塞尔曲线
+        contextRef.current.quadraticCurveTo(x, y, mx, my);
+
+        x = x2;
+        y = y2;
+      }
+    }
+    contextRef.current.lineTo(_p_list[_p_list.length - 1].x, _p_list[_p_list.length - 1].y);
+    contextRef.current.stroke();
+    contextRef.current.closePath();
+  };
+
+  /**
+   * @description: 通过2个坐标点，绘制直线
+   * @Param:
+   * @param {Point} a
+   * @param {Point} b
+   */
+  const drawStraightLineByPoint = (a: Point, b: Point) => {
+    contextRef.current.beginPath();
+
+    if (a && b) {
+      contextRef.current.lineWidth = 0.6;
+      contextRef.current.strokeStyle = '#111';
+
+      contextRef.current.moveTo(a.x, a.y);
+      contextRef.current.lineTo(b.x, b.y);
+
+      contextRef.current.stroke();
+    }
+
+    contextRef.current.closePath();
+  };
+  // #endregion
+
+  // const startDrawing = ({ nativeEvent }: any) => {
+  //   const { offsetX, offsetY } = nativeEvent;
+  //   contextRef.current.beginPath();
+  //   contextRef.current.moveTo(offsetX, offsetY);
+  //   setIsDrawing(true);
+  // };
+
+  // const finishDrawing = () => {
+  //   contextRef.current.closePath();
+  //   setIsDrawing(false);
+  // };
+
+  // const draw = ({ nativeEvent }: any) => {
+  //   if (isDrawing) {
+  //     const { offsetX, offsetY } = nativeEvent;
+  //     contextRef.current.lineTo(offsetX, offsetY);
+  //     contextRef.current.stroke();
+  //   }
+  // };
+
+  const clear_selection = () => {
+    contextRef.current.clearRect(
+      0,
+      0,
+      canvasRef.current.clientWidth,
+      canvasRef.current.clientHeight,
+    );
+
+    // 清除所有的坐标点
+    setP_list([]);
+  };
+
+  // #region 画布事件
+  const canvas_onMouseDown = ({ nativeEvent }: any) => {
+    const { offsetX, offsetY } = nativeEvent;
+    const p: Point = { x: offsetX, y: offsetY };
+
+    drawPoint(p);
+
+    if (p_list.length > 0) {
+      const dis = Math.sqrt(
+        (p_list[0].x - p.x) * (p_list[0].x - p.x) + (p_list[0].y - p.y) * (p_list[0].y - p.y),
+      );
+      console.log('dis', dis);
+      if (dis < 10) {
+        p_list.push(p_list[0]);
+      } else {
+        p_list.push(p);
+      }
+    } else {
+      p_list.push(p);
+    }
+
+    setP_list(p_list);
+    // if (p_list.length > 0) {
+    //   drawStraightLineByPoint(p_list[p_list.length - 2], p);
+    // }
+
+    if (p_list.length > 0) {
+      // 到起始点的距离
+
+      // 清空画布
+      contextRef.current.clearRect(0, 0, canvansSize.width, canvansSize.height);
+
+      // drawStraightLineByList(p_list);
+
+      drawSmoothLineByList(p_list);
+
+      // drawPointsLineByList(p_list);
+    }
+  };
+
+  const canvas_onMouseUp = ({ nativeEvent }: any) => {
+    const { offsetX, offsetY } = nativeEvent;
+    const p: Point = { x: offsetX, y: offsetY };
+  };
+
+  const canvas_onMouseMove = ({ nativeEvent }: any) => {
+    const { offsetX, offsetY } = nativeEvent;
+    const p: Point = { x: offsetX, y: offsetY };
+  };
+
+  const canvas_onMouseOut = ({ nativeEvent }: any) => {
+    const { offsetX, offsetY } = nativeEvent;
+    const p: Point = { x: offsetX, y: offsetY };
+  };
+  // #endregion
+
   return (
-    <div>
+    <div style={{ width: '1000px' }}>
       {/* <h1>图片处理工具</h1> */}
-      <nav id="PhotoTool-header-nav" className={styles.PhotoTool_header_nav}></nav>
-      <div style={{ display: 'flex' }}>
-        <nav id="PhotoTool-left-nav" className={styles.PhotoTool_left_nav}></nav>
-        <div className={styles.PhotoTool_photo_content}></div>
-        <nav id="PhotoTool-right-nav" className={styles.PhotoTool_right_nav}></nav>
+      <nav id="PhotoTool-header-nav" className={styles.PhotoTool_header_nav}>
+        <h1>图片处理工具</h1>
+        <p>当前图片:</p>
+        <Divider plain></Divider>
+        <Button type="primary" onClick={clear_selection}>
+          清除当前选区
+        </Button>
+      </nav>
+      <div style={{ display: 'flex', minHeight: '600px' }}>
+        <nav id="PhotoTool-left-nav" className={styles.PhotoTool_left_nav}>
+          <Divider plain>选择工具</Divider>
+          <Button type={tool === 1 ? 'primary' : 'default'} onClick={() => setTool(1)}>
+            矩形选择工具
+          </Button>
+          <Button type={tool === 2 ? 'primary' : 'default'} onClick={() => setTool(2)}>
+            圆形选择工具
+          </Button>
+          <Button type={tool === 3 ? 'primary' : 'default'} onClick={() => setTool(3)}>
+            椭圆选择工具
+          </Button>
+          <Button type={tool === 4 ? 'primary' : 'default'} onClick={() => setTool(4)}>
+            多边形选择工具
+          </Button>
+          <Button type={tool === 5 ? 'primary' : 'default'} onClick={() => setTool(5)}>
+            多边形套索工具
+          </Button>
+        </nav>
+        <div className={styles.PhotoTool_photo_content} ref={drawingBoardRef}>
+          <div
+            style={{
+              width: '80%',
+              maxHeight: '500px',
+              position: 'relative',
+              left: '10%',
+            }}
+          >
+            <canvas
+              id="PhotoTool-Canvas"
+              ref={canvasRef}
+              className={styles.PhotoTool_Canvas}
+              onMouseDown={canvas_onMouseDown}
+              onMouseUp={canvas_onMouseUp}
+              onMouseMove={canvas_onMouseMove}
+              onMouseOut={canvas_onMouseOut}
+            ></canvas>
+          </div>
+
+          {/* 绘制起始点 */}
+          {/* {p_list.length > 0 ? (
+            <div id="start-point" className={styles.PhotoTool_start_point}></div>
+          ) : null} */}
+        </div>
+        <nav id="PhotoTool-right-nav" className={styles.PhotoTool_right_nav}>
+          <Divider plain>选区属性</Divider>
+        </nav>
       </div>
+      <div id="PhotoTool-Console"></div>
     </div>
   );
 };
--- src/pages/Main/MainPage_SourceTag/index.js
+++ src/pages/Main/MainPage_SourceTag/index.js
@@ -2,232 +2,217 @@ import 'antd/dist/antd.css';
 import React, { useState } from 'react';
 import { Breadcrumb, Modal } from 'antd';
 import './PageTag.css';
-import pic1 from '../../static/pic1.png'
+import pic1 from '../../static/pic1.png';
 import { DatePicker, Space, Button } from 'antd';
 import { List, Tag, Table } from 'antd';
 
 // const { RangePicker } = DatePicker;
 
-const Picdata = [
-    '文件名称：阿巴阿巴阿巴',
-    '创建时间：2020.2.30',
-    '类型：图片',
-];
+const Picdata = ['文件名称：阿巴阿巴阿巴', '创建时间：2020.2.30', '类型：图片'];
 // const { Header, Content, Footer, Sider } = Layout;
 // const { SubMenu } = Menu;
 // const { Meta } = Card;
 
 const columns = [
-    {
-        title: '名称',
-        dataIndex: 'name',
-        key: 'name',
-        render: text => <a>{text}</a>,
-    },
-    {
-        title: '描述',
-        dataIndex: 'describe',
-        key: 'describe',
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (text, record) => (
-            <Space size="middle">
-                <a>操作 {record.key}</a>
-                <a>Delete</a>
-            </Space>
-        ),
-    },
+  {
+    title: '名称',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: '描述',
+    dataIndex: 'describe',
+    key: 'describe',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <Space size="middle">
+        <a>操作 {record.key}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
 ];
 
 const data = [];
 for (let i = 1; i <= 30; i++) {
-    data.push({
-        key: i,
-        name: '名称' + i,
-        describe: `第${i}个图片描述`
-    });
+  data.push({
+    key: i,
+    name: '名称' + i,
+    describe: `第${i}个图片描述`,
+  });
 }
 
 class ShowPic extends React.Component {
-    componentDidMount() {
-        // for (let i = 1; i <= 10; i++) {
-        //     var $div = $("<div></div>");
-        //     $div.attr('class', 'swiper-slide');
-        //     $div.attr('style', "background-image:url(http://112.124.18.109/M_test_images/nature-" + i + ".jpg)");
-        //     $('#galleryTop').append($div);
-        //     var $div2 = $("<div></div>");
-        //     $div2.attr('class', 'swiper-slide');
-        //     $div2.attr('style', "background-image:url(http://112.124.18.109/M_test_images/nature-" + i + ".jpg)");
-        //     $('#galleryThumbs').append($div2);
-        // }
+  componentDidMount() {
+    // for (let i = 1; i <= 10; i++) {
+    //     var $div = $("<div></div>");
+    //     $div.attr('class', 'swiper-slide');
+    //     $div.attr('style', "background-image:url(http://112.124.18.109/M_test_images/nature-" + i + ".jpg)");
+    //     $('#galleryTop').append($div);
+    //     var $div2 = $("<div></div>");
+    //     $div2.attr('class', 'swiper-slide');
+    //     $div2.attr('style', "background-image:url(http://112.124.18.109/M_test_images/nature-" + i + ".jpg)");
+    //     $('#galleryThumbs').append($div2);
+    // }
+    // var galleryThumbs = new Swiper('.gallery-thumbs', {
+    //     spaceBetween: 10,
+    //     slidesPerView: 4,
+    //     loop: true,
+    //     freeMode: true,
+    //     loopedSlides: 5, //looped slides should be the same
+    //     watchSlidesVisibility: true,
+    //     watchSlidesProgress: true,
+    // });
+    // var galleryTop = new Swiper('.gallery-top', {
+    //     spaceBetween: 10,
+    //     loop: true,
+    //     loopedSlides: 5, //looped slides should be the same
+    //     navigation: {
+    //         nextEl: '.swiper-button-next',
+    //         prevEl: '.swiper-button-prev',
+    //     },
+    //     thumbs: {
+    //         swiper: galleryThumbs,
+    //     },
+    // });
+  }
 
-        // var galleryThumbs = new Swiper('.gallery-thumbs', {
-        //     spaceBetween: 10,
-        //     slidesPerView: 4,
-        //     loop: true,
-        //     freeMode: true,
-        //     loopedSlides: 5, //looped slides should be the same
-        //     watchSlidesVisibility: true,
-        //     watchSlidesProgress: true,
-        // });
-        // var galleryTop = new Swiper('.gallery-top', {
-        //     spaceBetween: 10,
-        //     loop: true,
-        //     loopedSlides: 5, //looped slides should be the same
-        //     navigation: {
-        //         nextEl: '.swiper-button-next',
-        //         prevEl: '.swiper-button-prev',
-        //     },
-        //     thumbs: {
-        //         swiper: galleryThumbs,
-        //     },
-        // });
-    }
-
-    render() {
-        return (
-            <div className="root">
-                <div className="swiper-container gallery-top">
-                    <div className="swiper-wrapper" id="galleryTop">
-                    </div>
-                    <div className="swiper-button-next swiper-button-white"></div>
-                    <div className="swiper-button-prev swiper-button-white"></div>
-                </div>
-                <div className="swiper-container gallery-thumbs">
-                    <div className="swiper-wrapper" id="galleryThumbs">
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="root">
+        <div className="swiper-container gallery-top">
+          <div className="swiper-wrapper" id="galleryTop"></div>
+          <div className="swiper-button-next swiper-button-white"></div>
+          <div className="swiper-button-prev swiper-button-white"></div>
+        </div>
+        <div className="swiper-container gallery-thumbs">
+          <div className="swiper-wrapper" id="galleryThumbs"></div>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 class AddLabel extends React.Component {
-    state = { visible: false };
+  state = { visible: false };
 
-    showModal = () => {
-        this.setState({
-            visible: true,
-        });
-    };
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
 
-    handleOk = e => {
-        console.log(e);
-        this.setState({
-            visible: false,
-        });
-    };
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
-    handleCancel = e => {
-        console.log(e);
-        this.setState({
-            visible: false,
-        });
-    };
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <Button type="primary" onClick={this.showModal}>
-                    添加标签
-          </Button>
-                <Modal
-                    title="添加标签"
-                    visible={this.state.visible}
-                    onOk={this.handleOk}
-                    onCancel={this.handleCancel}
-                >
-                    <div style={{ marginBottom: 10 }}>
-                        <img src={pic1} />
-                    </div>
-                    <Tag color="magenta">magenta</Tag>
-                    <Tag color="red">red</Tag>
-                    <Tag color="volcano">volcano</Tag>
-                    <Tag color="orange">orange</Tag>
-                    <Tag color="gold">gold</Tag>
-                    <Tag color="lime">lime</Tag>
-                </Modal>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>
+          添加标签
+        </Button>
+        <Modal
+          title="添加标签"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <div style={{ marginBottom: 10 }}>
+            <img src={pic1} />
+          </div>
+          <Tag color="magenta">magenta</Tag>
+          <Tag color="red">red</Tag>
+          <Tag color="volcano">volcano</Tag>
+          <Tag color="orange">orange</Tag>
+          <Tag color="gold">gold</Tag>
+          <Tag color="lime">lime</Tag>
+        </Modal>
+      </div>
+    );
+  }
 }
-
 
 class PageTag extends React.Component {
-    state = {
-        collapsed: false,
-        bordered: false,
-        loading: false,
-        size: 'default',
-        title: undefined,
-        // rowSelection: {},
-        scroll: undefined,
-        hasData: true,
-        tableLayout: undefined,
-        top: 'none',
-        bottom: 'bottomRight',
-    };
+  state = {
+    collapsed: false,
+    bordered: false,
+    loading: false,
+    size: 'default',
+    title: undefined,
+    // rowSelection: {},
+    scroll: undefined,
+    hasData: true,
+    tableLayout: undefined,
+    top: 'none',
+    bottom: 'bottomRight',
+  };
 
-    onCollapse = collapsed => {
-        console.log(collapsed);
-        this.setState({ collapsed });
-    };
+  onCollapse = (collapsed) => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
 
-    render() {
-        const { xScroll, yScroll, ...state } = this.state;
-        const tableColumns = columns.map(item => ({ ...item, ellipsis: state.ellipsis }));
-        return (
-            <div>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item>活体检测</Breadcrumb.Item>
-                    <Breadcrumb.Item>数据标注</Breadcrumb.Item>
-                </Breadcrumb>
-                <div className="site-layout-content" style={{ padding: 24, minHeight: 360 }}>
-                    {/* <img className="picture" src={pic1} /> */}
-                    <div className="picture">
-                        <ShowPic />
-                    </div>
+  render() {
+    const { xScroll, yScroll, ...state } = this.state;
+    const tableColumns = columns.map((item) => ({ ...item, ellipsis: state.ellipsis }));
+    return (
+      <div>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>活体检测</Breadcrumb.Item>
+          <Breadcrumb.Item>数据标注</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content" style={{ padding: 24, minHeight: 360 }}>
+          {/* <img className="picture" src={pic1} /> */}
+          <div className="picture">
+            <ShowPic />
+          </div>
 
-                    <div className="List_ifo_1">
-                        <List
-                            // className='List_ifo'
-                            size="small"
-                            style={{ width: 300 }}
-                            bordered
-                            dataSource={Picdata}
-                            renderItem={item => <List.Item><h1>{item}</h1></List.Item>}
-                        />
+          <div className="List_ifo_1">
+            <List
+              // className='List_ifo'
+              size="small"
+              style={{ width: 300 }}
+              bordered
+              dataSource={Picdata}
+              renderItem={(item) => (
+                <List.Item>
+                  <h1>{item}</h1>
+                </List.Item>
+              )}
+            />
+          </div>
+          <div className="Tags_1">
+            <Tag color="magenta">magenta</Tag>
+            <Tag color="red">red</Tag>
+            <Tag color="volcano">volcano</Tag>
+            <Tag color="orange">orange</Tag>
+            <Tag color="gold">gold</Tag>
+            <Tag color="lime">lime</Tag>
+            <AddLabel />
+          </div>
 
-                    </div>
-                    <div className="Tags_1">
-                        <Tag color="magenta">magenta</Tag>
-                        <Tag color="red">red</Tag>
-                        <Tag color="volcano">volcano</Tag>
-                        <Tag color="orange">orange</Tag>
-                        <Tag color="gold">gold</Tag>
-                        <Tag color="lime">lime</Tag>
-                        <AddLabel />
-
-                    </div>
-
-
-                    <div style={{ marginTop: 550 }}>
-                        <Table
-                            {...this.state}
-
-                            className="SourceData"
-                            columns={columns} dataSource={data}
-                        />
-                    </div>
-
-                </div>
-
-            </div>
-        );
-    }
+          <div style={{ marginTop: 550 }}>
+            <Table {...this.state} className="SourceData" columns={columns} dataSource={data} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default PageTag
+export default PageTag;
